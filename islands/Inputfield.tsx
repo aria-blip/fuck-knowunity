22c7b445cdc32c76b26f507ef1e137aa84d46fba
Ari <aramehdin@gmail.com>
2025-03-19 21:18:49 +0100

    fixed button

--- islands/Inputfield.tsx
+++ islands/Inputfield.tsx
@@ -61,7 +61,11 @@ interface Props {
           textvalue.value =extractUUID(target.value)  } }  id="inputli" class="input-field" placeholder="Enter Link" />
 
 
-    <button class="search-button" onClick={ ()=> console.log( (document.getElementById("inputli") as HTMLInputElement).value)} >
+    <button class="search-button" onClick={ ()=> { 
+          textvalue.value =extractUUID(      (document.getElementById("inputli") as HTMLInputElement).value 
+        )   
+    }  
+  } >
       <svg class="search-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="#806600" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
         <circle cx="11" cy="11" r="8"></circle>
         <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
