66abb0115b7e55fdf486aa64a6c5bd174971abbc
Ari <aramehdin@gmail.com>
2025-03-21 23:02:18 +0100

    added footer

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -20,7 +20,13 @@ export default function App({ Component }: PageProps) {
         <Component />
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-
+        <footer class="footer mt-auto py-2" style="background: linear-gradient(90deg, #fffde7, #fff9c4); font-size: 0.8rem; position: fixed; bottom: 0; width: 100%; text-align: center; box-shadow: 0 -1px 3px rgba(0,0,0,0.1);">
+  <div class="container">
+    <span class="text-muted">
+      created by <a href="https://github.com/aria-blip/fuck-knowunity/blob/main/README.md" style="text-decoration: none; color: #b39800; font-weight: 500;">ARI</a>
+    </span>
+  </div>
+</footer>
       </body>
     </html>
   );
